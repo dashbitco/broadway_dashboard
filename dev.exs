7178707a6a13031cbc436b4706cff959375a0a8b
Philip Sampaio <philip.sampaio@gmail.com>
2021-05-05 15:32:12 -0300

    Add "dev" command to start a dashboard server
    This is useful to play with it locally. It is copied from
    PhoenixLiveDashboard project.

---
+++ dev.exs
@@ -0,0 +1,196 @@
+# iex -S mix dev
+Logger.configure(level: :debug)
+
+# Configures the endpoint
+Application.put_env(:phoenix_live_dashboard, DemoWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "Hu4qQN3iKzTV4fJxhorPQlA/osH9fAMtbtjVS58PFgfw3ja5Z18Q/WSNR9wP4OfW",
+  live_view: [signing_salt: "hMegieSe"],
+  http: [port: System.get_env("PORT") || 4000],
+  debug_errors: true,
+  check_origin: false,
+  pubsub_server: Demo.PubSub
+)
+
+defmodule Demo.Pipeline do
+  use Broadway
+
+  def start_link(opts) do
+    Broadway.start_link(__MODULE__,
+      name: __MODULE__,
+      producer: [
+        module: {Broadway.DummyProducer, opts},
+        concurrency: 1
+      ],
+      processors: [
+        default: [concurrency: 10]
+      ],
+      batchers: [
+        default: [batch_size: 20, concurrency: 4, batch_timeout: 2000],
+        s3: [concurrency: 3, batch_size: 15, batch_timeout: 2000]
+      ]
+    )
+  end
+
+  @impl true
+  def handle_message(_, %Broadway.Message{} = message, _) do
+    Broadway.Message.update_data(message, fn data ->
+      hex = Base.encode16(:crypto.strong_rand_bytes(64))
+
+      String.upcase(data <> hex)
+    end)
+    |> Broadway.Message.put_batcher(pick_batcher_key())
+  end
+
+  @impl true
+  def handle_batch(:default, messages, _, _) do
+    Enum.map(messages, fn message ->
+      Broadway.Message.update_data(message, fn data ->
+        String.downcase(data)
+      end)
+    end)
+  end
+
+  @impl true
+  def handle_batch(:s3, messages, _, _), do: messages
+
+  defp pick_batcher_key do
+    Enum.shuffle([:default, :s3]) |> List.first()
+  end
+end
+
+defmodule FakeProducer do
+  use GenServer
+
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, name: __MODULE__)
+  end
+
+  def init(_) do
+    timer = Process.send_after(self(), :publish, 1000)
+
+    {:ok, timer}
+  end
+
+  def handle_info(:publish, _timer) do
+    for i <- 1..1234, do: Broadway.test_message(Demo.Pipeline, "hello #{i}")
+
+    timer = Process.send_after(self(), :publish, 100)
+
+    {:noreply, timer}
+  end
+
+  def handle_info(_, timer) do
+    {:noreply, timer}
+  end
+
+  def terminate(_, timer) do
+    Process.cancel_timer(timer)
+
+    :ok
+  end
+end
+
+defmodule DemoWeb.PageController do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, :index) do
+    content(conn, """
+    <h2>BroadwayDashboard Dev</h2>
+    <a href="/dashboard" target="_blank">Open Dashboard</a>
+    """)
+  end
+
+  def call(conn, :hello) do
+    name = Map.get(conn.params, "name", "friend")
+    content(conn, "<p>Hello, #{name}!</p>")
+  end
+
+  defp content(conn, content) do
+    conn
+    |> put_resp_header("content-type", "text/html")
+    |> send_resp(200, "<!doctype html><html><body>#{content}</body></html>")
+  end
+end
+
+defmodule DemoWeb.Router do
+  use Phoenix.Router
+  import Phoenix.LiveDashboard.Router
+
+  pipeline :browser do
+    plug :fetch_session
+  end
+
+  scope "/" do
+    pipe_through :browser
+    get "/", DemoWeb.PageController, :index
+
+    live_dashboard("/dashboard",
+      env_keys: ["USER", "ROOTDIR"],
+      allow_destructive_actions: true,
+      additional_pages: [
+        broadway: {BroadwayDashboard, pipelines: [Demo.Pipeline, BikeSharing]}
+      ],
+      csp_nonce_assign_key: %{
+        img: :img_csp_nonce,
+        style: :style_csp_nonce,
+        script: :script_csp_nonce
+      }
+    )
+  end
+
+  def put_csp(conn, _opts) do
+    [img_nonce, style_nonce, script_nonce] =
+      for _i <- 1..3, do: 16 |> :crypto.strong_rand_bytes() |> Base.url_encode64(padding: false)
+
+    conn
+    |> assign(:img_csp_nonce, img_nonce)
+    |> assign(:style_csp_nonce, style_nonce)
+    |> assign(:script_csp_nonce, script_nonce)
+    |> put_resp_header(
+      "content-security-policy",
+      "default-src; script-src 'nonce-#{script_nonce}'; style-src 'nonce-#{style_nonce}'; " <>
+        "img-src 'nonce-#{img_nonce}' data: ; font-src data: ; connect-src 'self'; frame-src 'self' ;"
+    )
+  end
+end
+
+defmodule DemoWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :phoenix_live_dashboard
+
+  socket "/live", Phoenix.LiveView.Socket
+  socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
+
+  plug Phoenix.LiveReloader
+  plug Phoenix.CodeReloader
+
+  plug Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+
+  plug Plug.Session,
+    store: :cookie,
+    key: "_live_view_key",
+    signing_salt: "/VEDsdfsffMnp5"
+
+  plug Plug.RequestId
+  plug DemoWeb.Router
+end
+
+Application.ensure_all_started(:os_mon)
+Application.put_env(:phoenix, :serve_endpoints, true)
+
+Task.start(fn ->
+  children = [
+    {Phoenix.PubSub, [name: Demo.PubSub, adapter: Phoenix.PubSub.PG2]},
+    Demo.Pipeline,
+    FakeProducer,
+    DemoWeb.Endpoint
+  ]
+
+  {:ok, _} = Supervisor.start_link(children, strategy: :one_for_one)
+
+  Process.sleep(:infinity)
+end)
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule BroadwayDashboard.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps()
     ]
   end
@@ -24,8 +25,14 @@ defmodule BroadwayDashboard.MixProject do
     [
       {:phoenix_live_dashboard, github: "phoenixframework/phoenix_live_dashboard"},
       {:broadway, github: "dashbitco/broadway"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:plug_cowboy, "~> 2.0", only: :dev},
       {:jason, "~> 1.0", only: [:dev, :test, :docs]},
       {:floki, "~> 0.27.0", only: :test}
     ]
+  end
+
+  defp aliases do
+    [dev: "run --no-halt dev.exs"]
   end
 end
