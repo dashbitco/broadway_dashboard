832fbfd73135ef66b405ade8ca1457c19fa16a02
Philip Sampaio Silva <philip.sampaio@gmail.com>
2021-05-24 19:24:37 -0300

    Improve component test

--- test/broadway_dashboard/live_dashboard/layered_graph_component_test.exs
+++ test/broadway_dashboard/live_dashboard/layered_graph_component_test.exs
@@ -14,33 +14,26 @@ defmodule BroadwayDashboard.LiveDashboard.LayeredGraphComponentTest do
     :ok
   end
 
-  test "renders a basic pipeline" do
-    title = "my pipeline"
-    hint = "a Broadway pipeline represented as a graph"
-    layers = build_layers()
-
-    content = render_component(LayeredGraphComponent, layers: layers, hint: hint, title: title)
-
-    assert content =~ hint
-    assert content =~ title
-
-    assert content =~ "<line"
-    assert content =~ "<text"
-
+  defp circles_and_arrows_count(content) do
     fragment = Floki.parse_fragment!(content)
 
-    assert length(Floki.find(fragment, ".broadway-dashboard circle")) == 2
+    {
+      length(Floki.find(fragment, ".broadway-dashboard circle")),
+      length(Floki.find(fragment, ".broadway-dashboard line"))
+    }
   end
 
-  defp build_layers do
-    [
+  test "renders a basic broadway pipeline" do
+    title = "my pipeline"
+    hint = "a Broadway pipeline represented as a graph"
+
+    layers = [
       [
         %{
           id: MyPipeline.Broadway.Producer_0,
           data: %{
-            detail: 0,
             label: "prod_0",
-            show_detail?: false
+            detail: 0
           },
           children: [MyPipeline.Broadway.Processor_default_0]
         }
@@ -49,12 +42,42 @@ defmodule BroadwayDashboard.LiveDashboard.LayeredGraphComponentTest do
         %{
           id: MyPipeline.Broadway.Processor_default_0,
           data: %{
-            detail: 0,
+            detail: 1,
             label: "proc_1"
           },
           children: []
         }
       ]
     ]
+
+    format_detail = fn data ->
+      case data.detail do
+        0 -> "zero"
+        1 -> "one"
+        _ -> "n_n"
+      end
+    end
+
+    content =
+      render_component(LayeredGraphComponent,
+        layers: layers,
+        hint: hint,
+        title: title,
+        format_detail: format_detail
+      )
+
+    assert content =~ hint
+    assert content =~ title
+
+    assert content =~ "<line"
+    assert content =~ "<text"
+
+    assert content =~ "prod_0"
+    assert content =~ "zero"
+
+    assert content =~ "proc_1"
+    assert content =~ "one"
+
+    assert circles_and_arrows_count(content) == {2, 1}
   end
 end
