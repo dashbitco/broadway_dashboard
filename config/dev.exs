dcc0590fd9015f55eb90d3da97cc295bd732e40a
Philip Sampaio Silva <philip.sampaio@gmail.com>
2021-06-10 16:56:19 -0300

    Remove unused app config

--- config/dev.exs
+++ config/dev.exs
@@ -1,4 +1,1 @@
 use Mix.Config
-
-# Change "refresh mode" to `:manual` to stop auto-refresh of the page.
-config :broadway_dashboard, metrics_opts: [refresh_mode: :auto]
--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,1 @@
 use Mix.Config
-
-config :broadway_dashboard, metrics_opts: [refresh_mode: :manual]
