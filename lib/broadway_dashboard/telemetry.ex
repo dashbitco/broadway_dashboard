6f4202715dd460e4c04fec311c0f43ed9b874b16
louisvisser <59553710+louisvisser@users.noreply.github.com>
2022-12-13 20:05:03 +0200

    Improve Telemetry performance (#23)
    * Use function capture as event handler
    
    * Run formatter

--- lib/broadway_dashboard/telemetry.ex
+++ lib/broadway_dashboard/telemetry.ex
@@ -22,7 +22,12 @@ defmodule BroadwayDashboard.Telemetry do
       [:broadway, :batch_processor, :stop]
     ]
 
-    :telemetry.attach_many({__MODULE__, parent}, events, &handle_event/4, {pipeline, counters})
+    :telemetry.attach_many(
+      {__MODULE__, parent},
+      events,
+      &__MODULE__.handle_event/4,
+      {pipeline, counters}
+    )
   end
 
   def detach(parent) do
